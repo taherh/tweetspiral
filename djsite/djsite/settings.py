4a74b818d5d041dcf439822c490079b76eacb458
Taher Haveliwala <oss@taherh.org>
2022-04-06 17:55:35 -0700

    add template context preprocessor to fix STATIC_URL bug

--- djsite/djsite/settings.py
+++ djsite/djsite/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.static',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
